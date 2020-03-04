c950aa41ec636b560916f431b33e19aee36fb8a8
Prakhar Kumar <prakharkumar506978@gmail.com>
2020-03-04 23:15:36 +0530

    04-02-2020

--- Activity_Selection_Problem/Activity_Selection_Problem.py
+++ Activity_Selection_Problem/Activity_Selection_Problem.py
@@ -1,13 +1,13 @@
 start = list(map(int, input("Enter starting points of Activities :- ").rstrip().split()))
 finish = list(map(int, input("Enter finishing points of Activities :- ").rstrip().split()))
 obj = [i+1 for i in range(len(start))]
-newlist = sorted(zip(obj, start, finish), key=lambda x: x[2])
+new_list = sorted(zip(obj, start, finish), key=lambda x: x[2])
 result = list()
 result.append(1)
 j = 0
-for i in range(1, len(newlist)):
-    if newlist[i][1] >= newlist[j][2]:
-        result.append(newlist[i][0])
+for i in range(1, len(new_list)):
+    if new_list[i][1] >= new_list[j][2]:
+        result.append(new_list[i][0])
         j = i
 
 print("Resulting activities are :- {}".format(' '.join(list(map(str, result)))))
--- CoinChangeAlgorithm(Dynamic_Programming)/Python/CoinChangeAlgoOptimalSoln.py
+++ CoinChangeAlgorithm(Dynamic_Programming)/Python/CoinChangeAlgoOptimalSoln.py
@@ -1,4 +1,4 @@
-def count(a,m,n):
+def count(a, m, n):
     if n is 0:
         return 1
     if n < 0:
@@ -7,8 +7,9 @@ def count(a,m,n):
         return 0
     return count(a, m-1, n) + count(a, m, n-a[m-1])
 
+
 n = int(input("Enter total number of Coins :- "))
 print("Enter types of Coins")
 a = [int(input()) for i in range(n)]
 m = int(input("Enter total Denomination :- "))
-print("Result is :- {}".format(count(a,n,m)))
+print("Result is :- {}".format(count(a, n, m)))
